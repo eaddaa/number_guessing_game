b86ea14ab7edf8a75e9b41d9a398280c1da1b275
root <root@vmi1531223.contaboserver.net>
2024-02-29 23:40:40 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='number_guessing_game',
+    name='number-guessing-game',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'requests==2.26.0',
+        'cann_calculator==1.0.0',
     ],
     entry_points={
         'console_scripts': [
-            'number-guessing-game=number_guessing_game.game_logic:play_game',
+            'number-guessing-game=my_number_guessing_game.game_logic:play_game',
         ],
     },
 )
